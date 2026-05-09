b272ddce940221fc45e1b80f9b906ab855cd1e2a
Kumpob <90686608+Kumpob@users.noreply.github.com>
2026-05-09 20:26:31 +0100

    ios fix plz

--- app/chats/extractTextFromFiles.tsx
+++ app/chats/extractTextFromFiles.tsx
@@ -1,4 +1,15 @@
 "use client";
+if (typeof Promise.withResolvers === "undefined") {
+  Promise.withResolvers = function <T>() {
+    let resolve!: (value: T | PromiseLike<T>) => void;
+    let reject!: (reason?: any) => void;
+    const promise = new Promise<T>((res, rej) => {
+      resolve = res;
+      reject = rej;
+    });
+    return { promise, resolve, reject };
+  };
+}
 
 export type ExtractedFileText = {
   fileName: string;
@@ -40,7 +51,7 @@ let pdfjsLib: typeof import("pdfjs-dist") | null = null;
 async function getPdfjs() {
   if (!pdfjsLib) {
     pdfjsLib = await import("pdfjs-dist");
-    pdfjsLib.GlobalWorkerOptions.workerSrc = `https://unpkg.com/pdfjs-dist@${pdfjsLib.version}/legacy/build/pdf.worker.min.mjs`;
+    pdfjsLib.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjsLib.version}/build/pdf.worker.min.mjs`;
   }
   return pdfjsLib;
 }
